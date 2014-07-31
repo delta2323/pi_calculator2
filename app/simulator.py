dc1c84a41fc766ca04ab007d5219c7770175fde7
Kenta OONO <k.oono.delta@gmail.com>
2014-07-31 09:45:03 +0900

    Simulator support random seed

--- app/simulator.py
+++ app/simulator.py
@@ -4,6 +4,10 @@ def dist2(x, y):
     return x*x+y*y
 
 class Simulator:
+    def __init__(self, seed):
+        self.seed = seed
+        random.seed(seed)
+
     def simulate(self, sampling_num):
         inside_circle = 0
         for i in xrange(sampling_num):
@@ -11,8 +15,8 @@ class Simulator:
             if dist2(x, y) < 1 : inside_circle += 1
         return float(4*inside_circle) / sampling_num
 
-def main(sampling_num, output):
-    s = Simulator()
+def main(sampling_num, seed, output):
+    s = Simulator(seed)
     estimate = s.simulate(sampling_num)
     with open(output, 'w') as f:
         import json
@@ -22,5 +26,4 @@ if __name__ == "__main__":
     import sys
     print 'Arguments:'
     print sys.argv
-    main(int(sys.argv[1]), sys.argv[2])
-
+    main(int(sys.argv[1]), int(sys.argv[2]), sys.argv[3])
